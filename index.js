fe7107313413b4d1f9a97dd9a3cd3af346fb608f
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-02 14:11:00 +0200

    update kaydown event

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ dodger.style.bottom = "0px"; // sæt bund til 0px
 dodger.style.left = "0px"; // sæt dodger venstre til 0px
 
 document.addEventListener("keydown", function (event) {
-    console.log(event);
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
   });
