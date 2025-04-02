d28e20faf758e216d9830001ac51f8d0aa9f6ba7
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-02 14:09:38 +0200

    key event 1

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const dodger = document.getElementById("dodger");
 //styling i javascript
 dodger.style.backgroundColor = "#FF69B4"; // sæt baggrund til pink
 dodger.style.bottom = "0px"; // sæt bund til 0px
+dodger.style.left = "0px"; // sæt dodger venstre til 0px
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
