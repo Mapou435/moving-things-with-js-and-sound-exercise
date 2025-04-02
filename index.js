202cd4f3aab8186a6b446fd52a7294d8a597b2b8
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-02 14:43:13 +0200

    fix left
    dodger kan nu rykke til venstre

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ function moveDodgerLeft() {
     const left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
-      dodger.style.left = `${left - 10}px`;
-    }}
-
-function moveDodgerLeft() {
+      dodger.style.left = `${left - 10}px`; // Flytter dodger 10px til venstre
+    }
+  }
+  
+  function moveDodgerRight() {
     const rightNumbers = dodger.style.right.replace("px", "");
     const right = parseInt(rightNumbers, 10);
-      
-    if (right > 0) {
-        dodger.style.right = `${right - 10}px`;
-    }}
+  
+    if (right > 0) { 
+      dodger.style.right = `${right + 10}px`;
+    }
+  }
     
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
