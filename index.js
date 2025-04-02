2bca4f67ad7c9c6ab4eb0f0f7878646be4f2ec18
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-02 14:08:34 +0200

    bottom 0

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const dodger = document.getElementById("dodger");
 
 //styling i javascript
 dodger.style.backgroundColor = "#FF69B4"; // sæt baggrund til pink
-dodger.style.bottom = "100px"
-
+dodger.style.bottom = "0px"; // sæt bund til 0px
