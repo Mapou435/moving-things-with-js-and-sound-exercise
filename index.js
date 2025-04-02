6a4fce414ba7dfcedefc4739e4d8540f0dccaeeb
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-02 14:30:54 +0200

    update, not working move right left
    function for movedodger right og left lavet, men functionen virker ikke

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const dodger = document.getElementById("dodger");
 
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  }
+      dodger.style.left = `${left - 10}px`;
+    }}
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-       moveDodgerLeft();
-    }
-  
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-   });
+
+function moveDodgerLeft() {
+    const rightNumbers = dodger.style.right.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+      
+    if (right > 0) {
+        dodger.style.right = `${right - 10}px`;
+    }}
